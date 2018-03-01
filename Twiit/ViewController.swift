8a8315bf52385038def8807258ce71163de40e2c
Guilherme Coelho <guilherme@nexer.com.br>
2018-03-01 09:35:31 -0300

    Initial Commit

---
+++ Twiit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Twiit
+//
+//  Created by Guilherme Coelho on 3/1/18.
+//  Copyright © 2018 Guilherme Coelho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
